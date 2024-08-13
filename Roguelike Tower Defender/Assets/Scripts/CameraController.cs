f2523b9d96c434e8b4ef83002e9534df8070af2f
Daniel H <danielhesp06@gmail.com>
2024-08-13 12:00:46 +1200

    Camera v.1
    Working camera. Might add more features later.

---
+++ Roguelike Tower Defender/Assets/Scripts/CameraController.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float panBorderThickness;
+
+    [Header("Camera Move Speeds")]
+    public float panSpeed;
+    public float scrollSpeed;
+
+    [Header("Min and Max Values")]
+    public float minY;
+    public float maxY;
+    public float minX;
+    public float maxX;
+    public float minZ;
+    public float maxZ;
+
+    void Update()
+    {
+        if (GameManager.gameOver)
+        {
+            this.enabled = false;
+            return;
+        }
+
+        if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - panBorderThickness)
+        {
+            transform.Translate(Vector3.forward * panSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey("s") || Input.mousePosition.y <= panBorderThickness)
+        {
+            transform.Translate(Vector3.back * panSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - panBorderThickness)
+        {
+            transform.Translate(Vector3.right * panSpeed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey("a") || Input.mousePosition.x <= panBorderThickness)
+        {
+            transform.Translate(Vector3.left * panSpeed * Time.deltaTime, Space.World);
+        }
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+
+        Vector3 pos = transform.position;
+
+        pos.y -= scroll * 1000 * scrollSpeed * Time.deltaTime;
+
+        pos.x = Mathf.Clamp(pos.x, minX, maxX);
+        pos.y = Mathf.Clamp(pos.y, minY, maxY);
+        pos.z = Mathf.Clamp(pos.z, minZ, maxZ);
+
+        transform.position = pos;
+    }
+}
